c298fe4aeda067705e67729a7efb835070a8d527
Rubens Guimarães <rubens@epdvr.com.br>
2020-04-30 20:59:25 -0300

    Atualizações sem implicações para a aplicação

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,6 +5,12 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     outline: 0;
+    /* A propriedade box-sizing no CSS faz com que margin, padding e todos os outros espaçamentos
+       que podemos adicionar num elemento, eles sejam sempre somados a largura do elemento.
+       Por exemplo:
+       Se tivermos um elemento com 280px de largura fixa e adicionarmos 10px de padding, ele não passar a ocupar
+       300px, ele continua ocupando 280px, no entanto, o conteúdo desse elemento será exprimido para 260px,
+       que representa 260px + 10px de cada lado.  Isto é muito bom, pois evita algumas quebras de layout*/
     box-sizing: border-box;
   }
 
